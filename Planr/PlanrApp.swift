c9ddedce4c725ad9b8460ab141420d4b2f1fa00f
Mike Blakeman <Michael.Blakeman@garmin.com>
2021-08-25 09:07:30 -0500

    Adding initial project

---
+++ Planr/PlanrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanrApp.swift
+//  Planr
+//
+//  Created by Blakeman, Mike on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct PlanrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
